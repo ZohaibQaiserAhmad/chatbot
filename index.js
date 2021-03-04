14b05ab9bc82f234664294c19d32cc9a8f866d23
ZohaibQaiserAhmad <30640101+ZohaibQaiserAhmad@users.noreply.github.com>
2021-03-03 20:54:59 -0500

    make it better

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ bodyParser   = require('body-parser'),
 http         = require('http'),
 config       = require('./config'),
 server       = express(),
-var dotenv = require("dotenv");
+dotenv = require("dotenv");
 const {WebhookClient} = require('dialogflow-fulfillment');
 dotenv.config()
 
