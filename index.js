68ccd1c3ca81181b79f2263adc40b0952f7bdeb2
ZohaibQaiserAhmad <30640101+ZohaibQaiserAhmad@users.noreply.github.com>
2021-03-03 21:51:37 -0500

    make it better

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function universitySearch(req,res){
      */
 
     let questionToSearch = req;
-    res.send(SON.parse(req));
+    res.send(JSON.parse(req));
     exit(0);
 
     client.connect(() => {
