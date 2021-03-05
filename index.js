7ab1185ddb734410b5ba60243c68d60e435d341c
ZohaibQaiserAhmad <30640101+ZohaibQaiserAhmad@users.noreply.github.com>
2021-03-05 12:21:06 -0500

    make it better

--- index.js
+++ index.js
@@ -128,11 +128,24 @@ async function main(){
       await client.connect();
 
       // Make the appropriate DB calls
-      //await  universitySearch(client);
+      await  listDatabases(client);
 
   } catch (e) {
       console.error(e);
-  } finally {
-      await client.close();
-  }
+  } 
 }
+
+async function listDatabases(client){
+  //databasesList = await client.db().admin().listDatabases();
+  //console.log("Databases:");
+  //databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+  result = await client.db("Store").collection("University")
+                      .find({ }).
+                      toArray(function(err, result) {
+                          if (err) throw err;
+                          console.log(result);
+                          client.close();
+                        });
+};
+
+main().catch(console.error);
